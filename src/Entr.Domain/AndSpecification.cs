0c407d5eb8967316733ac31cc7b553ec540ea6f7
Bradley Ford <github@bradleyford.id.au>
2017-07-10 21:15:50 +1000

    Added specifications to Entr.Domain

---
+++ src/Entr.Domain/AndSpecification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Entr.Domain
+{
+    internal sealed class AndSpecification<T> : Specification<T>
+    {
+        public AndSpecification(Specification<T> left, Specification<T> right)
+            : base(CreateExpression(left, right))
+        {
+        }
+
+        static Expression<Func<T, bool>> CreateExpression(Specification<T> left, Specification<T> right)
+        {
+            var operand1 = left.ToExpression().Body;
+            var operand2 = right.ToExpression().Body;
+
+            var parameter = Expression.Parameter(typeof(T));
+
+            var body = Expression.AndAlso(operand1, operand2);
+            body = (BinaryExpression)new ExpressionParameterReplacer(parameter).Visit(body);
+
+            return Expression.Lambda<Func<T, bool>>(body, parameter);
+        }
+    }
+}
---
+++ src/Entr.Domain/Specification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Entr.Domain
+{
+    public abstract class Specification<T>
+    {
+        readonly Expression<Func<T, bool>> _expression;
+        readonly Lazy<Func<T, bool>> _delegate;
+
+        public Specification(Expression<Func<T, bool>> expression)
+        {
+            _expression = expression;
+            _delegate = new Lazy<Func<T, bool>>(() => _expression.Compile());
+        }
+
+        public bool IsSatisfiedBy(T value) => _delegate.Value(value);
+
+        public Expression<Func<T, bool>> ToExpression() => _expression;
+
+        public Specification<T> And(Specification<T> other) => new AndSpecification<T>(this, other);
+        public Specification<T> Or(Specification<T> other) => new OrSpecification<T>(this, other);
+        public Specification<T> Not() => new NotSpecification<T>(this);
+    }
+}
